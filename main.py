a0a21d7139139120fff7bc4380f3098ea7e1542a
Sergey <sstmail@mail.ru>
2019-04-06 22:04:14 +0300

    feat: add index route

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, abort, render_template, send_file
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index")
+def index():
+    return '<h1>Hello World!</h1>'
+app.add_url_rule('/', 'index', index)
+
+@app.route("/home")
 def home():
     return render_template("home.html")
 
