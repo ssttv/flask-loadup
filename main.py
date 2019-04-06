7f6c342b68d74fb4be25f352e6ac7ccd3da815f4
Sergey <sstmail@mail.ru>
2019-04-06 22:04:53 +0300

    feat: add user route with dynamic updates

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def index():
     return '<h1>Hello World!</h1>'
 app.add_url_rule('/', 'index', index)
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
 @app.route("/home")
 def home():
     return render_template("home.html")
